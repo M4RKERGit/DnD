84bbedb6995b853a0e4621bbcbb00ef4d23fb09e
M4RKERGit <62885141+M4RKERGit@users.noreply.github.com>
2020-06-24 01:57:13 +0300

    Hotfix

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata0[109];
+    QByteArrayData data[36];
+    char stringdata0[649];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,12 +38,56 @@ QT_MOC_LITERAL(2, 29, 0), // ""
 QT_MOC_LITERAL(3, 30, 20), // "on_to_save_triggered"
 QT_MOC_LITERAL(4, 51, 19), // "on_choice_triggered"
 QT_MOC_LITERAL(5, 71, 17), // "on_lock_triggered"
-QT_MOC_LITERAL(6, 89, 19) // "on_how_to_triggered"
+QT_MOC_LITERAL(6, 89, 19), // "on_how_to_triggered"
+QT_MOC_LITERAL(7, 109, 20), // "on_hotkeys_triggered"
+QT_MOC_LITERAL(8, 130, 17), // "on_d4x1_triggered"
+QT_MOC_LITERAL(9, 148, 17), // "on_d4x2_triggered"
+QT_MOC_LITERAL(10, 166, 17), // "on_d4x3_triggered"
+QT_MOC_LITERAL(11, 184, 17), // "on_d6x1_triggered"
+QT_MOC_LITERAL(12, 202, 17), // "on_d6x2_triggered"
+QT_MOC_LITERAL(13, 220, 17), // "on_d6x3_triggered"
+QT_MOC_LITERAL(14, 238, 17), // "on_d8x1_triggered"
+QT_MOC_LITERAL(15, 256, 17), // "on_d8x2_triggered"
+QT_MOC_LITERAL(16, 274, 17), // "on_d8x3_triggered"
+QT_MOC_LITERAL(17, 292, 18), // "on_d10x1_triggered"
+QT_MOC_LITERAL(18, 311, 18), // "on_d10x2_triggered"
+QT_MOC_LITERAL(19, 330, 18), // "on_d10x3_triggered"
+QT_MOC_LITERAL(20, 349, 18), // "on_d12x1_triggered"
+QT_MOC_LITERAL(21, 368, 18), // "on_d12x2_triggered"
+QT_MOC_LITERAL(22, 387, 18), // "on_d12x3_triggered"
+QT_MOC_LITERAL(23, 406, 18), // "on_d20x1_triggered"
+QT_MOC_LITERAL(24, 425, 18), // "on_d20x2_triggered"
+QT_MOC_LITERAL(25, 444, 18), // "on_d20x3_triggered"
+QT_MOC_LITERAL(26, 463, 19), // "on_d100x1_triggered"
+QT_MOC_LITERAL(27, 483, 19), // "on_d100x2_triggered"
+QT_MOC_LITERAL(28, 503, 19), // "on_d100x3_triggered"
+QT_MOC_LITERAL(29, 523, 17), // "slotShortcutCtrl4"
+QT_MOC_LITERAL(30, 541, 17), // "slotShortcutCtrl6"
+QT_MOC_LITERAL(31, 559, 17), // "slotShortcutCtrl8"
+QT_MOC_LITERAL(32, 577, 17), // "slotShortcutCtrl0"
+QT_MOC_LITERAL(33, 595, 17), // "slotShortcutCtrl1"
+QT_MOC_LITERAL(34, 613, 17), // "slotShortcutCtrl2"
+QT_MOC_LITERAL(35, 631, 17) // "slotShortcutCtrl3"
 
     },
     "MainWindow\0on_attack_clicked\0\0"
     "on_to_save_triggered\0on_choice_triggered\0"
-    "on_lock_triggered\0on_how_to_triggered"
+    "on_lock_triggered\0on_how_to_triggered\0"
+    "on_hotkeys_triggered\0on_d4x1_triggered\0"
+    "on_d4x2_triggered\0on_d4x3_triggered\0"
+    "on_d6x1_triggered\0on_d6x2_triggered\0"
+    "on_d6x3_triggered\0on_d8x1_triggered\0"
+    "on_d8x2_triggered\0on_d8x3_triggered\0"
+    "on_d10x1_triggered\0on_d10x2_triggered\0"
+    "on_d10x3_triggered\0on_d12x1_triggered\0"
+    "on_d12x2_triggered\0on_d12x3_triggered\0"
+    "on_d20x1_triggered\0on_d20x2_triggered\0"
+    "on_d20x3_triggered\0on_d100x1_triggered\0"
+    "on_d100x2_triggered\0on_d100x3_triggered\0"
+    "slotShortcutCtrl4\0slotShortcutCtrl6\0"
+    "slotShortcutCtrl8\0slotShortcutCtrl0\0"
+    "slotShortcutCtrl1\0slotShortcutCtrl2\0"
+    "slotShortcutCtrl3"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,7 +97,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+      34,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -61,13 +105,71 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       1,    0,  184,    2, 0x08 /* Private */,
+       3,    0,  185,    2, 0x08 /* Private */,
+       4,    0,  186,    2, 0x08 /* Private */,
+       5,    0,  187,    2, 0x08 /* Private */,
+       6,    0,  188,    2, 0x08 /* Private */,
+       7,    0,  189,    2, 0x08 /* Private */,
+       8,    0,  190,    2, 0x08 /* Private */,
+       9,    0,  191,    2, 0x08 /* Private */,
+      10,    0,  192,    2, 0x08 /* Private */,
+      11,    0,  193,    2, 0x08 /* Private */,
+      12,    0,  194,    2, 0x08 /* Private */,
+      13,    0,  195,    2, 0x08 /* Private */,
+      14,    0,  196,    2, 0x08 /* Private */,
+      15,    0,  197,    2, 0x08 /* Private */,
+      16,    0,  198,    2, 0x08 /* Private */,
+      17,    0,  199,    2, 0x08 /* Private */,
+      18,    0,  200,    2, 0x08 /* Private */,
+      19,    0,  201,    2, 0x08 /* Private */,
+      20,    0,  202,    2, 0x08 /* Private */,
+      21,    0,  203,    2, 0x08 /* Private */,
+      22,    0,  204,    2, 0x08 /* Private */,
+      23,    0,  205,    2, 0x08 /* Private */,
+      24,    0,  206,    2, 0x08 /* Private */,
+      25,    0,  207,    2, 0x08 /* Private */,
+      26,    0,  208,    2, 0x08 /* Private */,
+      27,    0,  209,    2, 0x08 /* Private */,
+      28,    0,  210,    2, 0x08 /* Private */,
+      29,    0,  211,    2, 0x08 /* Private */,
+      30,    0,  212,    2, 0x08 /* Private */,
+      31,    0,  213,    2, 0x08 /* Private */,
+      32,    0,  214,    2, 0x08 /* Private */,
+      33,    0,  215,    2, 0x08 /* Private */,
+      34,    0,  216,    2, 0x08 /* Private */,
+      35,    0,  217,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -88,6 +190,35 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_choice_triggered(); break;
         case 3: _t->on_lock_triggered(); break;
         case 4: _t->on_how_to_triggered(); break;
+        case 5: _t->on_hotkeys_triggered(); break;
+        case 6: _t->on_d4x1_triggered(); break;
+        case 7: _t->on_d4x2_triggered(); break;
+        case 8: _t->on_d4x3_triggered(); break;
+        case 9: _t->on_d6x1_triggered(); break;
+        case 10: _t->on_d6x2_triggered(); break;
+        case 11: _t->on_d6x3_triggered(); break;
+        case 12: _t->on_d8x1_triggered(); break;
+        case 13: _t->on_d8x2_triggered(); break;
+        case 14: _t->on_d8x3_triggered(); break;
+        case 15: _t->on_d10x1_triggered(); break;
+        case 16: _t->on_d10x2_triggered(); break;
+        case 17: _t->on_d10x3_triggered(); break;
+        case 18: _t->on_d12x1_triggered(); break;
+        case 19: _t->on_d12x2_triggered(); break;
+        case 20: _t->on_d12x3_triggered(); break;
+        case 21: _t->on_d20x1_triggered(); break;
+        case 22: _t->on_d20x2_triggered(); break;
+        case 23: _t->on_d20x3_triggered(); break;
+        case 24: _t->on_d100x1_triggered(); break;
+        case 25: _t->on_d100x2_triggered(); break;
+        case 26: _t->on_d100x3_triggered(); break;
+        case 27: _t->slotShortcutCtrl4(); break;
+        case 28: _t->slotShortcutCtrl6(); break;
+        case 29: _t->slotShortcutCtrl8(); break;
+        case 30: _t->slotShortcutCtrl0(); break;
+        case 31: _t->slotShortcutCtrl1(); break;
+        case 32: _t->slotShortcutCtrl2(); break;
+        case 33: _t->slotShortcutCtrl3(); break;
         default: ;
         }
     }
@@ -123,13 +254,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 34)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 34;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 34)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 34;
     }
     return _id;
 }
